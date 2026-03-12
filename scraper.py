7eaafbeb369dc8567c14a74c4bfb7d2733d30fde
daniellewamvumba-ctrl <daniellewamvumba@gmail.com>
2026-03-12 09:36:41 -0700

    Add book scraper for 50 pages
    it is a scraper designed to obtain books from books.toscrape.com using requests and Bs4

---
+++ scraper.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+BASE_URL = "https://books.toscrape.com/catalogue/page-{}.html"
+
+file = open("data/books.csv", "w", newline="", encoding="utf-8")
+writer = csv.writer(file)
+
+writer.writerow(["Title", "Price", "Rating"])
+
+for page in range(1, 51):
+
+    print(f"Scraping page {page}")
+
+    url = BASE_URL.format(page)
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = soup.find_all("article", class_="product_pod")
+
+    for book in books:
+
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+        rating = book.find("p", class_="star-rating")["class"][1]
+
+        writer.writerow([title, price, rating])
+
+file.close()
+
+print("Scraping finished. Data saved.")
